9f7b6e0cdef48b659676574b547e928b7529be94
wanghley <me@wanghley.com>
2024-06-15 17:08:23 -0500

    feat: intro video about summary and button click to about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,67 +1,63 @@
 import React, { useState, useEffect } from 'react'
 import about_photo from '../assets/about.jpg'
 import fetchTimeline from '../api/timeline'
-import { Timeline} from 'rsuite';
+import { Timeline } from 'rsuite';
 import './css/About.css'
-import ReactPlayer from 'react-player'
+
+import '@vidstack/react/player/styles/default/theme.css';
+import '@vidstack/react/player/styles/default/layouts/video.css';
+
+import { MediaPlayer, MediaProvider } from '@vidstack/react';
+import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
 
 const About = () => {
-    const [timeline, setTimeline] = useState(null);
-    const getTimeline = async () => {
-        try {
-            const data = await fetchTimeline();
-            setTimeline(data);
-        } catch (error) {
-            console.error("Error fetching data:", error);
-            // Handle the error as needed
-        }
-    };
-    useEffect(() => {
-        getTimeline();
-    }, []);
-    
-    return(<>
-            <div className='about'>
-                
-                <div className='about-text'>
-                    <h1 className='about-title'>About Me</h1>
-                    <h2 className='about-subtitle'>🎼 A Maestro of Curiosity 🚀 Crafting Tomorrow's Tech Marvels 🌟 One Ingenious Solution at a Time ⚙️ Shaping the Future, and Beyond! 🚀🔮</h2>
-                    <img src={about_photo} alt='Wanghley' className='about-photo-mobile lazyload'/>
+  const [timeline, setTimeline] = useState(null);
+  const getTimeline = async () => {
+    try {
+      const data = await fetchTimeline();
+      setTimeline(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      // Handle the error as needed
+    }
+  };
+  useEffect(() => {
+    getTimeline();
+  }, []);
 
-                    <p className='about-paragraph'>Wanghley is a relentless <strong>tech wizard</strong> rooted in the heart of Brasília, Brazil, and with an unconventional childhood spent on a pig farm 🐷. Now a dynamic aspirant <u>electrical and computer engineer</u> at Duke University, Wanghley is a <strong>curious problem-solver</strong> weaving his journey through computer science, electrical/biomedical engineering, and quantum computing.</p>
+  return (<>
+    <h1 className='about-title'>About Me</h1>
+    <div className='about'>
+      <div className='about-text'>
+        <h2 className='about-subtitle'>🎼 A Maestro of Curiosity 🚀 Crafting Tomorrow's Tech Marvels 🌟 One Ingenious Solution at a Time ⚙️ Shaping the Future, and Beyond! 🚀🔮</h2>
+        <img src={about_photo} alt='Wanghley' className='about-photo-mobile lazyload' />
 
-                    <p className='about-paragraph'>A <strong>Karsh Scholar @ Duke</strong> and a <strong>Líder Estudar Fellow @ Fundação Estudar</strong>, is not defined by accolades but by a mission. His toolbox? a blend of technical, management, soft skills – a symphony of skills cultivated in the pig farm and refined in the world of <u>science, engineering, and technology</u>.</p>
+        <p className='about-paragraph'>I'm Wanghley, a <strong>tech wizard</strong> from Brasília, Brazil, who grew up on a pig farm 🐷. Now an <u>electrical and computer engineering</u> student at Duke University, I'm exploring computer science, electrical/biomedical engineering, and quantum computing.</p>
 
-                    <p className='about-paragraph'>Beyond algorithms and circuits, he's a <strong>social entrepreneur</strong> with a flair for data science, a knack for innovation, and a passion for health tech. His projects include a frequency system with computer vision and a groundbreaking end-to-end solution for neurodegenerative diseases. These aren't just innovations; they're the echoes of a journey from pigsty to tech excellence.</p>
+        <p className='about-paragraph'>As a <strong>Karsh Scholar</strong> and <strong>Líder Estudar Fellow</strong>, I blend technical, management, and soft skills – from the pig farm to <u>science, engineering, and technology</u>.</p>
 
-                    <p className='about-paragraph'>Wanghley's narrative is a fusion of relentless ambition, a commitment to societal impact, and the magic of a pig farm childhood. Each breakthrough is not just a tech triumph; it's a promise of a future where his roots and wings coalesce into a story of <b>passion, problem-solving, and profound innovation</b>.</p>
+        <p className='about-paragraph'>I'm also a <strong>social entrepreneur</strong> with a passion for data science, innovation, and health tech. My projects include a frequency system with computer vision and a solution for neurodegenerative diseases.</p>
 
+        <p className='about-paragraph'>My story is one of ambition, societal impact, and the magic of a pig farm childhood, reflecting my <b>passion, problem-solving, and innovation</b>.</p>
 
-                    <button className='secondary-button about-button'>Know me more</button>
-                </div>
-                <div className='about-image'>
-                <ReactPlayer 
-                url='https://youtu.be/FOkCz0W5pgw'
-                playing={false}
-                controls={false}
-                volume={0.75}
-                config={
-                    { youtube: {
-                        playerVars: { 
-                            showinfo: 1,
-                            controls: 0,
-                            modestbranding: 1,
-                            loop: 1,
-                            rel: 0,
-                        }
-                    }}
-                }
-            />
-                    {/* <img src={about_photo} className='lazyload' alt='Wanghley'/> */}
-                </div>
-            </div>
-            </>
-    )
+        <button className='secondary-button about-button' onClick={() => window.location.href = '/about'}>Know me more</button>
+      </div>
+      <div className='about-image'>
+        <div className='video-container'>
+          <MediaPlayer playsInline
+            title="Wanghley: Innovator by Day, AI Ninja by Night"
+            src="youtube/FOkCz0W5pgw"
+            layout="video"
+            controls
+          >
+            <MediaProvider />
+            <DefaultVideoLayout thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt" icons={defaultLayoutIcons} />
+          </MediaPlayer>
+        </div>
+      </div>
+    </div>
+  </>
+  )
 }
 
 export default About
