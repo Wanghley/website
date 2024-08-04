39ddd45eb41338b1beefcb8ff4b3ab35d2334c7f
Wanghley Soares Martins <wanghleys@gmail.com>
2024-08-04 12:52:59 -0500

    feat: project page design

---
+++ src/utils/formatDate.js
@@ -0,0 +1,7 @@
+// src/utils/formatDate.js
+
+export const formatDate = (dateString) => {
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
+};
