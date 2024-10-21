6dc97c55ff1f749cdf9bb7a300e8583c89f0b8d4
wanghley <me@wanghley.com>
2024-10-21 00:43:23 -0400

    fix: project code inline material dark style

--- src/components/ProjectPost.js
+++ src/components/ProjectPost.js
@@ -5,9 +5,14 @@ import ReactMarkdown from 'react-markdown';
 import './css/ProjectPost.css';
 import { formatDate } from '../utils/formatDate';
 
+// import icon from react-icons
+import { FaTag } from "react-icons/fa";
+
+
 // Import components for specific Markdown elements
+
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { materialDark  } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const baseURL = process.env.REACT_APP_cms_base_url;
 const apiKey = process.env.REACT_APP_cms_api_token;
@@ -73,18 +78,68 @@ const ProjectPage = () => {
         td: ({ node, ...props }) => <td className="project-page__markdown-table-cell" {...props} />,
 
         // Render code blocks
-        code: ({ node, inline, className, children, ...props }) => {
-            const match = /language-(\w+)/.exec(className || '');
-            return inline ? (
-                <code className="project-page__markdown-inline-code" {...props}>
-                    {children}
-                </code>
-            ) : (
-                <SyntaxHighlighter style={solarizedlight} language={match ? match[1] : ''} {...props}>
-                    {String(children).replace(/\n$/, '')}
-                </SyntaxHighlighter>
-            );
-        },
+code: ({ node, inline, className, children, ...props }) => {
+    // Extract the language from the className
+    const match = /language-(\w+)/.exec(className || '');
+    const language = match ? match[1] : '';
+
+    // Inline code rendering
+    const renderInlineCode = () => (
+        <code className="project-page__markdown-inline-code" {...props}>
+            {children}
+        </code>
+    );
+
+    // Block code rendering
+    const renderBlockCode = () => (
+        <SyntaxHighlighter style={materialDark} language={language} {...props}>
+            {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+    );
+
+    return inline ? renderInlineCode() : renderBlockCode();
+},
+
+
+        // Render blockquotes
+        blockquote: ({ node, ...props }) => (
+            <blockquote className="project-page__markdown-blockquote" {...props} />
+        ),
+
+        // Render images
+        img: ({ node, alt, src, title }) => (
+            <img className="project-page__markdown-image" alt={alt} src={src} title={title} />
+        ),
+
+        // Render links
+        a: ({ node, ...props }) => (
+            <a className="project-page__markdown-link" {...props} />
+        ),
+
+        // Render horizontal rules
+        hr: ({ node, ...props }) => (
+            <hr className="project-page__markdown-horizontal-rule" {...props} />
+        ),
+
+        // Render details for collapsible sections
+        details: ({ node, ...props }) => (
+            <details className="project-page__markdown-details" {...props} />
+        ),
+
+        // Render summary for collapsible sections
+        summary: ({ node, ...props }) => (
+            <summary className="project-page__markdown-summary" {...props} />
+        ),
+
+        // Render task lists
+        taskList: ({ node, ...props }) => (
+            <ul className="project-page__markdown-task-list" {...props} />
+        ),
+
+        // Render task list items
+        taskListItem: ({ node, ...props }) => (
+            <li className="project-page__markdown-task-list-item" {...props} />
+        ),
     };
 
     return (
@@ -96,7 +151,10 @@ const ProjectPage = () => {
                         <div className="project-page__overlay">
                             <h1 className="project-page__title">{Title}</h1>
                             <div className="project-page__info">
-                                <span className="project-page__category">{Category}</span>
+                                <span className="project-page__category">
+                                    <FaTag />
+                                    {Category}
+                                </span>
                                 <span className="project-page__dates">
                                     {Start ? formatDate(Start) : 'N/A'}
                                     {End && <span><strong> - </strong>{formatDate(End)}</span>}
