e7d770ee1c631ce4329d178bd2d5b80bcbd9e303
wanghley <me@wanghley.com>
2025-01-22 19:49:47 -0500

    feat: experience list and filtering CV implemented

--- src/components/cv/ExperienceList.js
+++ src/components/cv/ExperienceList.js
@@ -1,24 +1,70 @@
 // components/ExperienceList.js
 import React from 'react';
 
+import '../css/ExperienceList.css';
+
 const ExperienceList = ({ experiences }) => {
     return (
         <section className="experience">
             <h2>Experience</h2>
-            <ul>
-                {experiences.map((experience) => {
-                    return (
-                        <div key={experience.id}>
-                            <h3>{experience.attributes.title}</h3>
-                            <p>{experience.attributes.company}</p>
-                            <p>{experience.attributes.location?.description}</p>
-                            <p>{experience.attributes.start} - {experience.attributes.end}</p>
+
+            {/* Industry Experiences */}
+            <div className="experience-category">
+                <h3>Industry Experiences</h3>
+                <ul>
+                    {experiences.filter(exp => exp.attributes.type === 'Industry').map((experience) => (
+                        <li key={experience.id}>
+                            <h4>{experience.attributes.title}</h4>
+                            <div className="experience-details">
+                                <span className="company">{experience.attributes.company}</span>
+                                <span className="location">{experience.attributes.location?.description}</span>
+                                <span className="dates">{experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} - {experience.attributes.end.split('-')[1]}/{experience.attributes.end.split('-')[0]}</span>
+                            </div>
                             <p>{experience.attributes.description}</p>
-                        </div>
-                    );
-                })}
-            </ul>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            {/* Research Experiences */}
+            <div className="experience-category">
+                <h3>Research Experiences</h3>
+                <ul>
+                    {experiences.filter(exp => exp.attributes.type === 'Research').map((experience) => (
+                        <li key={experience.id}>
+                            <h4>{experience.attributes.title}</h4>
+                            <div className="experience-details">
+                                <span className="company">{experience.attributes.company}</span>
+                                <span className="location">{experience.attributes.location?.description}</span>
+                                {/* show year and month */}
+                                <span className="dates">{experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} - {experience.attributes.end.split('-')[1]}/{experience.attributes.end.split('-')[0]}</span>
+                            </div>
+                            <p>{experience.attributes.description}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            {/* Teaching & Mentorship Experiences */}
+            <div className="experience-category">
+                <h3>Teaching & Mentorship Experiences</h3>
+                <ul>
+                    {experiences.filter(exp => exp.attributes.type === 'Teaching & Mentorship').map((experience) => (
+                        <li key={experience.id}>
+                            <h4>{experience.attributes.title}</h4>
+                            <div className="experience-details">
+                                <span className="company">{experience.attributes.company}</span>
+                                <span className="location">{experience.attributes.location?.description}</span>
+                                <span className="dates">{experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} - {experience.attributes.end.split('-')[1]}/{experience.attributes.end.split('-')[0]}</span>
+                            </div>
+                            <p>{experience.attributes.description}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </section>
+
+
     );
 };
 
