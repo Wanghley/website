b3c0496069a88a54e1e557f3665e1dbc13a0dd54
Wanghley Soares Martins <wanghleys@gmail.com>
2025-02-02 13:00:34 -0500

    fix: skills highlighting compacted

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,22 +1,28 @@
 import React, { useState, useEffect, useRef } from "react";
 import SkillsRadarChart from "./SkillsRadarChart";
 import "./css/Skills.css";
-import { Tabs, Tab, Paper } from '@mui/material';
 import { getUniqueElements, fetchFeaturedSkills } from "../api/skills";
 import SkillCard from "./SkillCard";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import { CustomLeftArrow, CustomRightArrow } from './CustomArrows'; // Import custom arrows
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { CustomLeftArrow, CustomRightArrow } from "./CustomArrows";
+
+const breakpoints = {
+    superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 6 }, // Show more items
+    desktop: { breakpoint: { max: 3000, min: 1024 }, items: 4 },  // Show more items
+    tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
+    mobile: { breakpoint: { max: 464, min: 0 }, items: 1 }
+};
 
 const Skills = () => {
-    const [featuredSkillsData, setFeaturedSkillsData] = useState([]);
+    const [featuredSkills, setFeaturedSkills] = useState([]);
     const containerRef = useRef(null);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const data = await fetchFeaturedSkills();
-                setFeaturedSkillsData(data);
+                setFeaturedSkills(data);
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
@@ -24,80 +30,54 @@ const Skills = () => {
         fetchData();
     }, []);
 
-    const breakpoints = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    };
-
-    const getItemsInView = () => {
-        const width = window.innerWidth;
-        if (width > 3000) {
-            return 5;
-        } else if (width >= 1024) {
-            return 3;
-        } else if (width > 464) {
-            return 2;
-        } else {
-            return 1;
-        }
-    };
+    const getFilteredSkills = (type) => featuredSkills.filter(skill => skill.type === type);
 
     const renderCategory = (title, filterType) => {
-        const items = featuredSkillsData.filter(skill => skill.type === filterType);
-        const itemsInView = getItemsInView();
+        const skills = getFilteredSkills(filterType);
+        const shouldAutoPlay = skills.length > breakpoints.desktop.items;
 
-        return (
+        return skills.length ? (
             <div className="skills_category">
                 <h2>{title}</h2>
                 <div className="skills_card">
                     <Carousel
                         responsive={breakpoints}
-                        infinite={true}
-                        autoPlay={items.length > itemsInView}
+                        infinite
+                        autoPlay={shouldAutoPlay}
                         autoPlaySpeed={3000}
                         arrows
                         customLeftArrow={<CustomLeftArrow />}
                         customRightArrow={<CustomRightArrow />}
-                        showDots={false}
-                        swipeable={true}
-                        draggable={true}
+                        swipeable
+                        draggable
                     >
-                        {items.map((skill, index) => (
+                        {skills.map((skill, index) => (
                             <SkillCard key={index} title={skill.name} icon={skill.icon} />
                         ))}
                     </Carousel>
                 </div>
             </div>
-        );
+        ) : null;
     };
 
     return (
-        <div ref={containerRef}>
-            <div className="skills">
-                <h1>Featured Skills</h1>
-                <div className="skills_container">
-                    {renderCategory("Engineering", "Engineering")}
-                    {renderCategory("Dev", "Software")}
-                    {renderCategory("DevOps", "DevOps")}
-                    {renderCategory("Data", "Data")}
-                    {renderCategory("Other", "Other")}
-                </div>
+        <div ref={containerRef} className="skills">
+            <h1>Featured Skills</h1>
+            <div className="skills_container">
+                {renderCategory("Engineering", "Engineering")}
+                {renderCategory("Software Development", "Software")}
+                {renderCategory("DevOps", "DevOps")}
+                {renderCategory("Data Science", "Data")}
+                {renderCategory("Other", "Other")}
             </div>
+
+            {/* Button to see more on CV */}
+            {/* <div className="cv-button-container">
+                <a href="/path-to-cv" className="cv-button">See More on My CV</a>
+            </div> */}
+            <button className='secondary-button about-button' onClick={() => window.location.href = '/curriculum-vitae'}>See more</button>
         </div>
+
     );
 };
 
