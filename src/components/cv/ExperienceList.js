a8169593943b0f053801b40742ee05df86b7aedb
Wanghley Soares Martins <wanghleys@gmail.com>
2025-02-09 21:37:20 -0500

    feat: add volunteer experiences section to CV

--- src/components/cv/ExperienceList.js
+++ src/components/cv/ExperienceList.js
@@ -62,6 +62,24 @@ const ExperienceList = ({ experiences }) => {
                     ))}
                 </ul>
             </div>
+
+            {/* Volunteer Experiences */}
+            <div className="experience-category">
+                <h3>Volunteer Experiences</h3>
+                <ul>
+                    {experiences.filter(exp => exp.attributes.type === 'Volunteering').map((experience) => (
+                        <li key={experience.id}>
+                            <h4>{experience.attributes.title}</h4>
+                            <div className="experience-details">
+                                <span className="company">{experience.attributes.company}</span>
+                                <span className="location">{experience.attributes.location?.description}</span>
+                                <span className="dates">{experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} - {experience.attributes.end.split('-')[1]}/{experience.attributes.end.split('-')[0]}</span>
+                            </div>
+                            <p>{experience.attributes.description}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </section>
 
 
