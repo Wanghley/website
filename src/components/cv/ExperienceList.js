1c7e9905ddbb43eb82a0fcc35dac1e0b82d7520f
Wanghley Soares Martins <wanghleys@gmail.com>
2025-07-11 14:58:32 -0400

    feat: enhance ExperienceList component with ReactMarkdown for description rendering and improve layout with flexbox

--- src/components/cv/ExperienceList.js
+++ src/components/cv/ExperienceList.js
@@ -2,6 +2,7 @@
 import React from 'react';
 
 import '../css/ExperienceList.css';
+import ReactMarkdown from 'react-markdown';
 
 const ExperienceList = ({ experiences }) => {
     return (
@@ -14,15 +15,25 @@ const ExperienceList = ({ experiences }) => {
                 <ul>
                     {experiences.filter(exp => exp.attributes.type === 'Industry').map((experience) => (
                         <li key={experience.id}>
-                            <h4>{experience.attributes.title}</h4>
                             <div className="experience-details">
-                                <span className="company">{experience.attributes.company}</span>
-                                <span className="location">{experience.attributes.location?.description}</span>
-                                <span className="dates">{experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} - {experience.attributes.end
-                                    ? `${experience.attributes.end.split('-')[1]}/${experience.attributes.end.split('-')[0]}`
-                                    : 'Present'} </span>
+                                <div className="company-date-row">
+                                    <span className="company">{experience.attributes.company}</span>
+                                    <span className="dates">
+                                        {experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} -
+                                        {experience.attributes.end
+                                            ? `${experience.attributes.end.split('-')[1]}/${experience.attributes.end.split('-')[0]}`
+                                            : 'Present'}
+                                    </span>
+                                </div>
+                                <div className="location-row">
+                                    <h4>{experience.attributes.title}</h4>
+                                    <span className="location">{experience.attributes.location?.description}</span>
+                                </div>
                             </div>
-                            <p>{experience.attributes.description}</p>
+
+                            <ReactMarkdown className="description">
+                                {experience.attributes.description}
+                            </ReactMarkdown>
                         </li>
                     ))}
                 </ul>
@@ -34,16 +45,24 @@ const ExperienceList = ({ experiences }) => {
                 <ul>
                     {experiences.filter(exp => exp.attributes.type === 'Research').map((experience) => (
                         <li key={experience.id}>
-                            <h4>{experience.attributes.title}</h4>
                             <div className="experience-details">
-                                <span className="company">{experience.attributes.company}</span>
-                                <span className="location">{experience.attributes.location?.description}</span>
-                                {/* show year and month */}
-                                <span className="dates">{experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} - {experience.attributes.end
-                                    ? `${experience.attributes.end.split('-')[1]}/${experience.attributes.end.split('-')[0]}`
-                                    : 'Present'}</span>
+                                <div className="company-date-row">
+                                    <span className="company">{experience.attributes.company}</span>
+                                    <span className="dates">
+                                        {experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} -
+                                        {experience.attributes.end
+                                            ? `${experience.attributes.end.split('-')[1]}/${experience.attributes.end.split('-')[0]}`
+                                            : 'Present'}
+                                    </span>
+                                </div>
+                                <div className="location-row">
+                                    <h4>{experience.attributes.title}</h4>
+                                    <span className="location">{experience.attributes.location?.description}</span>
+                                </div>
                             </div>
-                            <p>{experience.attributes.description}</p>
+                            <ReactMarkdown className="description">
+                                {experience.attributes.description}
+                            </ReactMarkdown>
                         </li>
                     ))}
                 </ul>
@@ -55,15 +74,24 @@ const ExperienceList = ({ experiences }) => {
                 <ul>
                     {experiences.filter(exp => exp.attributes.type === 'Teaching & Mentorship').map((experience) => (
                         <li key={experience.id}>
-                            <h4>{experience.attributes.title}</h4>
                             <div className="experience-details">
-                                <span className="company">{experience.attributes.company}</span>
-                                <span className="location">{experience.attributes.location?.description}</span>
-                                <span className="dates">{experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} - {experience.attributes.end
-                                    ? `${experience.attributes.end.split('-')[1]}/${experience.attributes.end.split('-')[0]}`
-                                    : 'Present'}</span>
+                                <div className="company-date-row">
+                                    <span className="company">{experience.attributes.company}</span>
+                                    <span className="dates">
+                                        {experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} -
+                                        {experience.attributes.end
+                                            ? `${experience.attributes.end.split('-')[1]}/${experience.attributes.end.split('-')[0]}`
+                                            : 'Present'}
+                                    </span>
+                                </div>
+                                <div className="location-row">
+                                    <h4>{experience.attributes.title}</h4>
+                                    <span className="location">{experience.attributes.location?.description}</span>
+                                </div>
                             </div>
-                            <p>{experience.attributes.description}</p>
+                            <ReactMarkdown className="description">
+                                {experience.attributes.description}
+                            </ReactMarkdown>
                         </li>
                     ))}
                 </ul>
@@ -75,15 +103,24 @@ const ExperienceList = ({ experiences }) => {
                 <ul>
                     {experiences.filter(exp => exp.attributes.type === 'Volunteering').map((experience) => (
                         <li key={experience.id}>
-                            <h4>{experience.attributes.title}</h4>
                             <div className="experience-details">
-                                <span className="company">{experience.attributes.company}</span>
-                                <span className="location">{experience.attributes.location?.description}</span>
-                                <span className="dates">{experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} - {experience.attributes.end
-                                    ? `${experience.attributes.end.split('-')[1]}/${experience.attributes.end.split('-')[0]}`
-                                    : 'Present'}</span>
+                                <div className="company-date-row">
+                                    <span className="company">{experience.attributes.company}</span>
+                                    <span className="dates">
+                                        {experience.attributes.start.split('-')[1]}/{experience.attributes.start.split('-')[0]} -
+                                        {experience.attributes.end
+                                            ? `${experience.attributes.end.split('-')[1]}/${experience.attributes.end.split('-')[0]}`
+                                            : 'Present'}
+                                    </span>
+                                </div>
+                                <div className="location-row">
+                                    <h4>{experience.attributes.title}</h4>
+                                    <span className="location">{experience.attributes.location?.description}</span>
+                                </div>
                             </div>
-                            <p>{experience.attributes.description}</p>
+                            <ReactMarkdown className="description">
+                                {experience.attributes.description}
+                            </ReactMarkdown>
                         </li>
                     ))}
                 </ul>
